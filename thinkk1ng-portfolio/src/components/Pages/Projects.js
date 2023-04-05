db39f83b8747e8ea981cd4a0d196c42788b9092b
ThinkK1ng <cla.givens@gmail.com>
2023-04-04 20:28:24 -0500

    added third project

--- thinkk1ng-portfolio/src/components/Pages/Projects.js
+++ thinkk1ng-portfolio/src/components/Pages/Projects.js
@@ -1,42 +1,40 @@
 import React from 'react'
 import gamingApp from '../images/Gaming App Project.png';
 import cassetteTape from '../images/The Cassette Tape.png';
+import Rappor from '../images/Rappor.png';
 import Portfolio from '../PortfolioContainer';
-// https://github.com/hmustain/The-Cassette-Tape
-// https://the-cassette-tape.herokuapp.com
-// https://thinkk1ng.github.io/Gaming-APP-Project/
-// https://thinkk1ng.github.io/Gaming-APP-Project/
 
 
 
 const projects = [
   {
     id: 1,
-    image: {gamingApp},
-    name: "Twitch Popularity Tracker",
-    alt: "Twitch Popularity Tracker",
-    description: "",
-    repo: "https://github.com/ThinkK1ng/Gaming-APP-Project",
-    link: "https://thinkk1ng.github.io/Gaming-APP-Project/",
+    image: { gamingApp },
+    name: 'Twitch Popularity Tracker',
+    alt: 'Twitch Popularity Tracker',
+    description: '',
+    repo: 'https://github.com/ThinkK1ng/Gaming-APP-Project',
+    link: 'https://thinkk1ng.github.io/Gaming-APP-Project/',
   },
   {
     id: 2,
-    image: {cassetteTape},
-    name: "The Cassette Tape",
-    alt: "The Cassette Tape",
-    description: "",
-    repo: "https://github.com/hmustain/The-Cassette-Tape",
-    link: "https://the-cassette-tape.herokuapp.com",
+    image: { cassetteTape },
+    name: 'The Cassette Tape',
+    alt: 'The Cassette Tape',
+    description: '',
+    repo: 'https://github.com/hmustain/The-Cassette-Tape',
+    link: 'https://the-cassette-tape.herokuapp.com',
   },
   {
     id: 3,
-    image: "",
-    name: "Project 3",
-    alt: "",
-    description: "",
-    link: "",
+    image: { Rappor },
+    name: 'Project 3',
+    alt: 'Rappor',
+    description: '',
+    repo: 'https://github.com/Carter-Schmitz/Rappor',
+    link: 'https://rappor-social-media.herokuapp.com',
   },
-];
+]
 
 export default function Projects() {
   return (
